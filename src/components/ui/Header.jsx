81ff444555a757e3348f19acc0a6e20ea215dc47
Shreya904 <shreyasrivastava92006@gmail.com>
2024-08-11 21:15:00 +0530

    modified header

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,7 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { IconHome, IconMessage, IconUser, IconCode } from "@tabler/icons-react";
 import { cn } from "../lib/utils";
+
 export function HeaderSection() {
+  const location = useLocation();
+
   const navItems = [
     {
       name: "Home",
@@ -26,25 +29,28 @@ export function HeaderSection() {
       ),
     },
   ];
+
   return (
     <div className="relative py-2 w-full">
-      <div className="flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-white/[0.2] rounded-full bg-black shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4">
+      <div className="flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-white/[0.2] rounded-full bg-black shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-8 py-2  items-center justify-center space-x-4">
         {navItems.map((navItem, idx) => (
           <Link
             key={`link=${idx}`}
             to={navItem.link}
             className={cn(
-              "relative text-neutral-50 items-center flex space-x-1 hover:text-neutral-300 "
+              "relative text-neutral-50 items-center flex space-x-1 hover:text-neutral-300 px-4 py-2 rounded-full",
+              location.pathname === navItem.link
+                ? "border text-sm font-medium border-white/[0.2] text-white"
+                : ""
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
             <span className="hidden sm:block text-sm">{navItem.name}</span>
+            {location.pathname === navItem.link && (
+              <span className="absolute  w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
+            )}
           </Link>
         ))}
-        {/* <button className="border text-sm font-medium relative border-white/[0.2] text-white px-4 py-2 rounded-full">
-          <span>Login</span>
-          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-        </button> */}
       </div>
     </div>
   );
